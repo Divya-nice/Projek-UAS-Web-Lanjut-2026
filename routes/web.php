6ac689da66b08c6015bdfbab17aa9b47bb9afff3
natasya helmalia <natasyahelmalia886@gmail.com>
2026-06-07 20:00:07 +0700

    feat: backend login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Laravel berjalan';
+});
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return "Selamat datang di Dashboard";
+    });
+
 });
