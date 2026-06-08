dbaebde2bc7df335510bfad7c65a84eb80c5a883
241230021-commits <241230021@unmuhpnk.ac.id>
2026-06-08 11:20:43 +0700

    Pesan update register ui

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('register');
-});
+})->name('register');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
