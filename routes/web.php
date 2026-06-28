10c227bf61b544f589250371efeef630809967bd
Nabila Septi <nabila@example.com>
2026-06-28 14:35:28 +0700

    Menambahkan halaman Jadwal Saya beserta tampilan riwayat kegiatan relawan

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::view('/relawan', 'relawan.index');
 
 Route::view('/relawan/detail', 'relawan.show');
 Route::view('/relawan/sukses', 'relawan.sukses');
+Route::view('/relawan/jadwal', 'relawan.jadwal');
