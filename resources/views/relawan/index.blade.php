a13c519477cc9fa5cce5cc5219a199f50b688568
Divya <241230011@unmuhpnk.ac.id>
2026-07-08 01:43:30 +0700

    Perbaikan navbar

--- resources/views/relawan/index.blade.php
+++ resources/views/relawan/index.blade.php
@@ -27,6 +27,7 @@ justify-between
 items-center
 shadow-lg">
 
+    <!-- Logo -->
     <div class="flex items-center gap-3">
         <img src="{{ asset('images/buku.png') }}" class="w-8">
         <h1 class="font-bold text-2xl">
@@ -34,73 +35,22 @@ shadow-lg">
         </h1>
     </div>
 
-<div class="flex items-center gap-3">
-
-<img src="{{ asset('images/buku.png') }}"
-
-class="w-8">
-
-<h1 class="font-bold text-2xl">
-
-GemaAksara
-
-</h1>
-
-</div>
-
-<div class="flex gap-14 font-medium">
-
-<a href="/relawan"
-
-class="hover:text-yellow-300">
-
-Beranda Kegiatan
-
-</a>
-
-
-<a href="">
-
-Jadwal Saya
-
-</a>
-
-</div>
-
-<div class="flex items-center gap-6">
-
-    <div class="flex items-center">
-
-        <span class="font-medium">
-            Halo, Relawan
-        </span>
-
-    </div>
-
-    <form action="/logout" method="POST">
-        @csrf
-
-        <button
-            type="submit"
-            class="bg-white text-amber-900 px-4 py-2 rounded-xl font-semibold hover:bg-amber-100 transition">
-
-            Logout
-
-        </button>
-
-    </form>
+    <!-- Menu -->
     <div class="flex gap-14 font-medium">
 
-        <a href="/relawan" class="text-yellow-300">
+        <a href="/relawan"
+           class="text-yellow-300">
             Beranda Kegiatan
         </a>
 
-        <a href="/relawan/jadwal" class="hover:text-yellow-300 transition">
+        <a href="/relawan/jadwal"
+           class="hover:text-yellow-300 transition">
             Jadwal Saya
         </a>
 
     </div>
 
+    <!-- User -->
     <div class="flex items-center gap-6">
 
         <div class="flex items-center">
